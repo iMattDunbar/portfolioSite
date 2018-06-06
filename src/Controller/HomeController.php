c82aa1464c61ab7674d6a4041c4b33fd6ace15eb
Matt Dunbar <matt@fusioncorpdesign.com>
2018-06-05 23:18:11 -0400

    Database created with doctrine.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,10 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //return $this->render('home/test.html.twig');
         return $this->render('home/index.html.twig');
+
+        //return $this->redirect('http://www.mattdunbar.io');
     }
 
 
